c5a9a2fde0d4380312d3734d0d6752341b174fd2
Artem Burov <45521325+burovarte@users.noreply.github.com>
2023-06-24 21:21:04 +0300

    mainpage

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { MainContent } from "./components/mainContent/MainContent";
+
+function App() {
+	return <MainContent />;
+}
+
+export default App;
---
+++ src/components/mainContent/MainContent.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import headerLogo from "../../assets/Illustration.svg";
+import styles from "./MainContent.module.css";
+
+export const MainContent = () => {
+	return (
+		<div className={styles.container}>
+			<div className={styles.content}>
+				<img src={headerLogo} className={styles.headerLogo} alt="logo" />
+			</div>
+			<div className={styles.textContent}>
+				<h1>Добро пожаловать в конструктор!</h1>
+				<p>Выберите действие для продолжения</p>
+			</div>
+			<div className={styles.buttonsContent}>
+				<button className={styles.whiteButton}>Изменить конфигурацию</button>
+				<button className={styles.blackButton}>Купить любой диван</button>
+			</div>
+		</div>
+	);
+};
